e6b8629787a5d60cb0ad9f62be336c1d52546741
emernemly <nemlyemer@gmail.com>
2022-01-05 23:17:58 +0100

    cards

---
+++ Desktop/card_react/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import "./Navbar.css";
+let Nav=()=>{
+ return(
+  <ul className="topnav">
+  <li><a className="active" href="Home.html">Home</a></li>
+  <li><a href="Page1.html">Page1</a></li>
+  <li><a href="Page2.html">Page filtre</a></li>
+  <li className="right" ><a href="About.html">About</a></li>
+</ul>
+ )
+}
+export default Nav
